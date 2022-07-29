9457519612d3ab50a2d4c35d131737ac91ee3e8c
unknown <sergeyandmelnikov@gmail.com>
2022-07-29 23:25:44 +0300

    entry point

--- vpooh-adventure/src/app/game.js
+++ vpooh-adventure/src/app/game.js
@@ -1,15 +1,19 @@
 
 // Variables
 let game = document.querySelector('#game');
+let player = document.querySelector('.player');
 
 let player = {
-	x: 0,
-	y: 0,
-	el: false
-}
+	x: 50,
+	y: 50,
+	el: false,
+};
 
 
 // Entry point
-function startGame(event) {
-	//...
+function startGame() {
+	game.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
+	player.el = true;
 }
+
+startGame();
