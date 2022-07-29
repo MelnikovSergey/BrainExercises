c612ec680b07757aed5d3ab3a39314a142007ee2
unknown <sergeyandmelnikov@gmail.com>
2022-07-29 23:30:30 +0300

    added move player func

--- vpooh-adventure/src/app/game.js
+++ vpooh-adventure/src/app/game.js
@@ -1,19 +1,28 @@
 
 // Variables
 let game = document.querySelector('#game');
-let player = document.querySelector('.player');
+let playerBlock = document.querySelector('.player');
 
 let player = {
 	x: 50,
 	y: 50,
-	el: false,
+	el: false
 };
 
+// Move player
+function movePlayer() {
+	document.addEventListener('keydown', (event) => {
+		console.log(event.keyCode);
+	});
+}
 
 // Entry point
 function startGame() {
 	game.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
 	player.el = true;
+
+	// Init func
+	movePlayer();
 }
 
 startGame();
