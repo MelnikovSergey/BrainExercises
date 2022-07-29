78fe89bd9ecf7afc473552c19c2f51e747a39fbd
unknown <sergeyandmelnikov@gmail.com>
2022-07-29 23:40:58 +0300

    added event listener keys

--- vpooh-adventure/src/app/game.js
+++ vpooh-adventure/src/app/game.js
@@ -1,7 +1,7 @@
 
 // Variables
 let game = document.querySelector('#game');
-let playerBlock = document.querySelector('.player');
+let playerContainer = null;
 
 let player = {
 	x: 50,
@@ -12,13 +12,41 @@ let player = {
 // Init resources
 function initResources() {
 	game.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
-	player.el = true;
+	playerContainer = document.querySelector('.player');
+	player.el = true; 
 }
 
 // Move player
 function movePlayer() {
 	document.addEventListener('keydown', (event) => {
 		console.log(event.keyCode);
+
+		switch (event.keyCode)
+		{
+		  case 38: 
+			// up
+		  	playerContainer.style.top = `${player.y -= 5}px`;
+		    	break;
+
+		  case 40:
+			// down  
+		    	playerContainer.style.top = `${player.y += 5}px`;
+		    break;
+
+		  case 39:
+			// left 
+		  	playerContainer.style.left = `${player.x += 5}px`;
+		    	break;
+
+		  case 37:
+			// right
+		  	playerContainer.style.left = `${player.x -= 5}px`;		    
+		    break;
+
+		  default:
+			//..		    
+		    break;
+		}
 	});
 }
 
