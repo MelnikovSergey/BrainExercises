ce6182c1b99082175e1faa809adb2c43ed701982
unknown <sergeyandmelnikov@gmail.com>
2022-07-29 23:44:53 +0300

    update move player func

--- vpooh-adventure/src/app/game.js
+++ vpooh-adventure/src/app/game.js
@@ -6,41 +6,44 @@ let playerContainer = null;
 let player = {
 	x: 50,
 	y: 50,
-	el: false
+	step: 5
 };
 
 // Init resources
 function initResources() {
 	game.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
 	playerContainer = document.querySelector('.player');
-	player.el = true; 
 }
 
 // Move player
 function movePlayer() {
 	document.addEventListener('keydown', (event) => {
-		console.log(event.keyCode);
+		// console.log(event.keyCode);
 
 		switch (event.keyCode)
 		{
 		  case 38: 
 			// up
-		  	playerContainer.style.top = `${player.y -= 5}px`;
+			player.y -= player.step;
+		  	playerContainer.style.top = `${player.y}px`;
 		    	break;
 
 		  case 40:
-			// down  
-		    	playerContainer.style.top = `${player.y += 5}px`;
+			// down
+			player.y += player.step;  
+		    	playerContainer.style.top = `${player.y}px`;
 		    break;
 
 		  case 39:
-			// left 
-		  	playerContainer.style.left = `${player.x += 5}px`;
+			// left
+			player.x += player.step; 
+		  	playerContainer.style.left = `${player.x}px`;
 		    	break;
 
 		  case 37:
 			// right
-		  	playerContainer.style.left = `${player.x -= 5}px`;		    
+			player.y -= player.step;
+		  	playerContainer.style.left = `${player.x}px`;		    
 		    break;
 
 		  default:
