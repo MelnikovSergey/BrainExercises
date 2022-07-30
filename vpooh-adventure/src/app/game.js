0ca03138a6e96dbf0dd53995900afe74e2435df6
unknown <sergeyandmelnikov@gmail.com>
2022-07-30 23:52:19 +0300

    move player feature refactoring

--- vpooh-adventure/src/app/game.js
+++ vpooh-adventure/src/app/game.js
@@ -1,62 +1,118 @@
+(function(id) {
+	'use strict';
 
-// Variables
-let game = document.querySelector('#game');
-let playerContainer = null;
+	// Const
+	const FPS = 1000 / 60;
+	const keyCodeArray = [38, 40, 39, 37];
+	
+	// Variables
+	let game = document.querySelector('#game');
+	let playerContainer = null;
+	
+	let player = {
+		x: 50,
+		y: 50,
+		step: 5,
+		move: false,
+		direction: null // 1 - up, 2 - down, 3 - left, 4 - right 
+	};
+	
+	let intervals = {
+		run: false
+	}
+	
+	// Init resources
+	function initResources() {
+		game.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
+		playerContainer = document.querySelector('.player');
+	}
+	
+	// Init intervals
+	function initIntervals() {
+		intervals.run = setInterval(() => {
+	
+			if(player.move === true) {
+	
+				switch (player.direction)
+				{
+			  	  case 1: 
+					// up
+					player.y -= player.step;
+			  		playerContainer.style.top = `${player.y}px`;
+			    		break;
+	
+			  	  case 2: 
+					// down
+					player.y += player.step;  
+			    		playerContainer.style.top = `${player.y}px`;
+			   		break;
+	
+			  	  case 3: 
+					// left
+					player.x += player.step; 
+			  		playerContainer.style.left = `${player.x}px`;
+			    		break;
+	
+			  	  case 4: 
+					// right
+					player.x -= player.step;
+			  		playerContainer.style.left = `${player.x}px`;		    
+					break;
+				}
+			}
+	
+		}, FPS);
+	}
+	
+	// Move player
+	function movePlayer() {
+		document.addEventListener('keydown', (event) => {
+	
+			switch (event.keyCode)
+			{
+			  case 38: 
+				player.move = true;
+				player.direction = 1;
+				console.log('Move up');
+				break;
+	
+			  case 40:
+				player.move = true;
+				player.direction = 2;
+				console.log('Move down');
+				break;
+	
+			  case 39:
+				player.move = true;
+				player.direction = 3;
+				console.log('Move left');
+				break;
+	
+			  case 37:
+				player.move = true;
+				player.direction = 4;
+				console.log('Move right');
+				break;
+			}
+		});
+	
+		document.addEventListener('keyup', (event) => {
+			
+			if(keyCodeArray.includes(event.keyCode)) {
+				player.move = false;
+				console.log('KeyUp ' + event.keyCode);
+			}
+		
+		});
+	}
+	
+	// Entry point
+	function startGame() {
+		initResources();
+		initIntervals();
+		movePlayer();
+	}
+	
+	startGame();
 
-let player = {
-	x: 50,
-	y: 50,
-	step: 5
-};
-
-// Init resources
-function initResources() {
-	game.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
-	playerContainer = document.querySelector('.player');
-}
-
-// Move player
-function movePlayer() {
-	document.addEventListener('keydown', (event) => {
-		// console.log(event.keyCode);
-
-		switch (event.keyCode)
-		{
-		  case 38: 
-			// up
-			player.y -= player.step;
-		  	playerContainer.style.top = `${player.y}px`;
-		    	break;
-
-		  case 40:
-			// down
-			player.y += player.step;  
-		    	playerContainer.style.top = `${player.y}px`;
-		    break;
-
-		  case 39:
-			// left
-			player.x += player.step; 
-		  	playerContainer.style.left = `${player.x}px`;
-		    	break;
-
-		  case 37:
-			// right
-			player.y -= player.step;
-		  	playerContainer.style.left = `${player.x}px`;		    
-		    break;
-
-		  default:
-			//..		    
-		    break;
-		}
-	});
-}
-
-// Entry point
-function startGame() {
-	initResources();
-	movePlayer();
-}
-
-startGame();
+}(document));
